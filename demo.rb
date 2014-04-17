0eb55154e5d44afe778762553da0f50728e77d59
Michael Sorm <michaelsorm12@gmail.com>
2014-04-17 14:30:19 +1200

    update

--- demo.rb
+++ demo.rb
@@ -4,8 +4,12 @@ def say msg
 	puts "=> #{msg} "
 end
 
-def push_result
-	puts "Your result is #{$result}"
+def push_result outcome
+	puts "Your result is #{outcome}"
+end
+
+def options
+	say "1) Add, 2) Subtract, 3) Multiply, 4) Divide "
 end
 
 say "What's the first number?"
@@ -14,23 +18,23 @@ num1 = gets.chomp
 say "What's the second number?"
 num2 = gets.chomp
 
-say "1) Add, 2) Subtract, 3) Multiply, 4) Divide "
+options
 operator = gets.chomp
 
 if operator == "1"
-	$result = num1.to_i + num2.to_i
-	push_result
+	result = num1.to_i + num2.to_i
+	push_result(result)
 elsif operator == "2"
 	result = num1.to_i - num2.to_i
-	puts "Your result is #{result}"
+	push_result(result)
 elsif operator == "3"
 	result = num1.to_i * num2.to_i
-	puts "Your result is #{result}"
+	push_result(result)
 elsif operator == "4"
 	result = num1.to_f / num2.to_f
-	puts "Your result is #{result}"
+	push_result(result)
 else
 	puts "Please try again"
-	say	"1) Add, 2) Subtract, 3) Multiply, 4) Divide "
+	options
 end	
 	
